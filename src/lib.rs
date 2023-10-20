394833f65769d71f0b01953d5cc9426ed8b2a972
Kurt Bartnik <kbartnik@gmail.com>
2023-10-20 17:58:26 -0400

    Add initial NaiveBayesClassifier implementation.

--- src/lib.rs
+++ src/lib.rs
@@ -1,14 +1,68 @@
-pub fn add(left: usize, right: usize) -> usize {
-    left + right
+use regex::Regex;
+use std::collections::{HashMap, HashSet};
+
+pub fn tokenize(lower_case_text: &str) -> HashSet<&str> {
+    Regex::new(r"[a-z0-9']+")
+        .unwrap()
+        .find_iter(lower_case_text)
+        .map(|mat| mat.as_str())
+        .collect()
 }
 
-#[cfg(test)]
-mod tests {
-    use super::*;
+pub struct Message<'a> {
+    pub text: &'a str,
+    pub is_spam: bool,
+}
 
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
+pub struct NaiveBayesClassifier {
+    pub alpha: f64,
+    pub tokens: HashSet<String>,
+    pub token_ham_counts: HashMap<String, i32>,
+    pub token_spam_counts: HashMap<String, i32>,
+    pub ham_messages_count: i32,
+    pub spam_messages_count: i32,
+}
+
+impl NaiveBayesClassifier {
+    pub fn train(&mut self, messages: &[Message]) {
+        for message in messages.iter() {
+            self.increment_message_classifications_count(message);
+            for token in tokenize(&message.text.to_lowercase()) {
+                self.tokens.insert(token.to_string());
+                self.increment_token_count(token, message.is_spam)
+            }
+        }
+    }
+
+    fn increment_message_classifications_count(&mut self, message: &Message) {
+        if message.is_spam {
+            self.spam_messages_count += 1;
+        } else {
+            self.ham_messages_count += 1;
+        }
+    }
+
+    fn increment_token_count(&mut self, token: &str, is_spam: bool) {
+        if !self.token_spam_counts.contains_key(token) {
+            self.token_spam_counts.insert(token.to_string(), 0);
+        }
+
+        if !self.token_ham_counts.contains_key(token) {
+            self.token_ham_counts.insert(token.to_string(), 0);
+        }
+
+        if is_spam {
+            self.increment_spam_count(token);
+        } else {
+            self.increment_ham_count(token);
+        }
+    }
+
+    fn increment_spam_count(&mut self, token: &str) {
+        *self.token_spam_counts.get_mut(token).unwrap() += 1;
+    }
+
+    fn increment_ham_count(&mut self, token: &str) {
+        *self.token_ham_counts.get_mut(token).unwrap() += 1;
     }
 }

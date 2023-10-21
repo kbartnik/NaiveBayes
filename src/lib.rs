e21b211e58095ce0925591528f472f509f56fcd9
Kurt Bartnik <kbartnik@gmail.com>
2023-10-21 12:33:09 -0400

    Add predictive logic to NaiveBayseClassifier implementation.

--- src/lib.rs
+++ src/lib.rs
@@ -65,4 +65,55 @@ impl NaiveBayesClassifier {
     fn increment_ham_count(&mut self, token: &str) {
         *self.token_ham_counts.get_mut(token).unwrap() += 1;
     }
+
+    pub fn predict(&self, text: &str) -> f64 {
+        let lower_case_text = text.to_lowercase();
+        let message_tokens = tokenize(&lower_case_text);
+        let (prob_if_spam, prob_if_ham) = self.probabilities_of_message(message_tokens);
+
+        prob_if_spam / (prob_if_spam + prob_if_ham)
+    }
+
+    fn probabilities_of_message(&self, message_tokens: HashSet<&str>) -> (f64, f64) {
+        let mut log_prob_if_spam = 0.0;
+        let mut log_prob_if_ham = 0.0;
+
+        for token in self.tokens.iter() {
+            let (prob_if_spam, prob_if_ham) = self.probabilities_of_token(token);
+
+            if message_tokens.contains(token.as_str()) {
+                log_prob_if_spam += prob_if_spam.ln();
+                log_prob_if_ham += prob_if_ham.ln();
+            } else {
+                log_prob_if_spam += (1.0 - prob_if_spam).ln();
+                log_prob_if_ham += (1.0 - prob_if_ham).ln();
+            }
+        }
+
+        let prob_if_spam = log_prob_if_spam.exp();
+        let prob_if_ham = log_prob_if_ham.exp();
+
+        (prob_if_spam, prob_if_ham)
+    }
+
+    fn probabilities_of_token(&self, token: &str) -> (f64, f64) {
+        let prob_of_token_spam = (self.token_spam_counts[token] as f64 + self.alpha)
+            / (self.spam_messages_count as f64 + 2.0 * self.alpha);
+
+        let prob_of_token_ham = (self.token_ham_counts[token] as f64 + self.alpha)
+            / (self.ham_messages_count as f64 + 2.0 * self.alpha);
+
+        (prob_of_token_spam, prob_of_token_ham)
+    }
+
+    pub fn new(alpha: f64) -> Self {
+        Self {
+            alpha,
+            tokens: HashSet::new(),
+            token_spam_counts: HashMap::new(),
+            token_ham_counts: HashMap::new(),
+            spam_messages_count: 0,
+            ham_messages_count: 0,
+        }
+    }
 }
